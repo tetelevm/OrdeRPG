d242663255842edd1568f2e9e7e8e1ebe8e3a5d8
tetelevm <tetelev99@mail.ru>
2021-07-27 11:34:30 +0300

    Preparation for User

--- server/db/models/user.py
+++ server/db/models/user.py
@@ -1,8 +1,17 @@
-from server.utils.db import BaseModel
+from server.utils.db import BaseModel, StringField, DateTimeField
+from server.settings import settings
 
 
 __all__ = ['UserModel']
 
 
 class UserModel(BaseModel):
-    pass
+    login = StringField(string_length=50)
+    pass_hash = StringField()
+    pepper = StringField(48)
+    token = StringField(128)
+    last_login = DateTimeField()
+
+    @staticmethod
+    def hash_password(password, salt, pepper):
+        return settings.password_hasher(password, salt, pepper)
