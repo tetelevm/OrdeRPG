5b75e815917af64dc0e8a83153e581e95b6d53c9
tetelevm <tetelev99@mail.ru>
2021-07-29 15:58:52 +0300

    Update UserModel due to new fields

--- server/db/models/user.py
+++ server/db/models/user.py
@@ -1,17 +1,13 @@
-from server.utils.db import BaseModel, StringField, DateTimeField
-from server.settings import settings
-
+from server.utils.db import (
+    BaseModel, StringField, DateTimeField, RandomStringField, PasswordField,
+)
 
 __all__ = ['UserModel']
 
 
 class UserModel(BaseModel):
     login = StringField(string_length=50)
-    pass_hash = StringField()
-    pepper = StringField(48)
-    token = StringField(128)
+    pass_hash = PasswordField()
+    pepper = RandomStringField(48)
+    token = RandomStringField(128)
     last_login = DateTimeField()
-
-    @staticmethod
-    def hash_password(password, salt, pepper):
-        return settings.password_hasher(password, salt, pepper)
