e902117a5346c9b9e471e4c9e9bc079c2ea788e0
tetelevm <tetelev99@mail.ru>
2021-08-06 17:45:50 +0300

    Refactor EnvParser realization

--- server/utils/lib/classes/env_parser.py
+++ server/utils/lib/classes/env_parser.py
@@ -9,98 +9,193 @@ from typing import Union, Iterable, Mapping
 from ..exceptions import ExceptionFromFormattedDoc
 from .singleton import Singleton
 
+
 _all_ = ['env_parser']
 
 __all__ = _all_ + ['EnvParser']
 
 
 STR_OR_ITER = Union[str, Iterable[str]]
+FROM_STRING_TYPE = tuple[Union[tuple[str, str], tuple], bool]
+
+DEFAULT_OBJ = object()
 
 
 class EnvParser(Singleton):
-    # Incorrect string in the variable file
-    class IncorrectStringError(ExceptionFromFormattedDoc):
-        """ File "{}" str {}: string <{}> is incorrect """
+    """
+    A class that collects and defines an environment variable.
 
-    # If the argument from the environment variables is incorrect
-    # For example: `object`, `list()`, `[a % 2 for a in [1, 2, 3, 4]]`
-    class IncorrectArgumentValueError(ExceptionFromFormattedDoc):
-        """ The "{}" env argument cannot have the value <{}> """
+    The main method of class is `get_arg_from_dict`, which searches and
+    translates into python-view the necessary values.
+
+    It also has an important method `get_arg_from_file` in which it reads
+    the specified file and searches it for desired values.
+    """
+
+    class IncorrectStringError(ExceptionFromFormattedDoc):
+        """ Incorrect string in the variable file. """
+        __doc__ = """ File "{}" str {}: string <{}> is incorrect """
 
     def __init__(self):
         self._files_cache = dict()
 
-    @classmethod
-    def _get_arg_from_dict(cls, names: STR_OR_ITER, args_dict: Mapping, default=object):
-        correct_name = ''
-        value = default
+    @staticmethod
+    def get_arg_from_dict(
+            names: STR_OR_ITER,
+            args_dict: Mapping[str, str],
+            default: any = DEFAULT_OBJ,
+            translate: bool = True,
+    ) -> any:
+        """
+        Searches for one or more variable names in the dictionary, and
+        then translates the variable value from string to Python value.
+
+        >>> EnvParser.get_arg_from_dict('foo', {'foo': '100'})
+        >>> # 100
+        >>>
+        >>> EnvParser.get_arg_from_dict(
+        >>>    ['foo', 'bar'], {'bar': '{"a": 1, "b": 2}'})
+        >>> # {'a': 1, 'b': 2}
+        >>>
+        >>> EnvParser.get_arg_from_dict(['foo', 'bar'], {'spam': '100'})
+        >>> # NameError: "['foo', 'bar']" arguments not found
+        >>>
+        >>> EnvParser.get_arg_from_dict('foo', {'bar': '100'}, 300)
+        >>> # 300
+        >>>
+        >>> EnvParser.get_arg_from_dict('foo', {'foo': 'bar'})
+        >>> # ValueError: The "foo" env argument cannot have value <bar>
+        >>>
+        >>> EnvParser.get_arg_from_dict(
+        >>>     'foo', {'foo': 'bar'}, translate=False)
+        >>> # 'bar'
+        >>>
+        >>> EnvParser.get_arg_from_dict(
+        >>>     'foo', {'bar': 'spam'}, 'eggs', translate=False)
+        >>> # 'eggs'
+        """
 
         if isinstance(names, str):
             names = [names]
+        # Names must be only strings
+        names = [str(name) for name in names]
 
         for maybe_name in names:
-            if not isinstance(maybe_name, str):
-                raise ValueError(
-                    f'{maybe_name} argument must be a <str>, not a {type(maybe_name)}')
+            if maybe_name not in args_dict:
+                continue
 
-        for maybe_name in names:
-            value = args_dict.get(maybe_name, object)
-            if value is not object:
-                correct_name = maybe_name
-                break
+            correct_name = maybe_name
+            value = args_dict[maybe_name]
 
-        if value is object:
-            if len(names) == 1:
-                raise NameError(f'"{names[0]}" argument not found')
-            else:
-                raise NameError(f'"{names}" arguments not found')
+            if translate:
+                try:
+                    value = ast.literal_eval(value)
+                except ValueError:
+                    # Argument from the environment variables is incorrect
+                    # Example: `object`, `list()`, `[a % 2 for a in [1, 2, 3]]`
+                    error = 'The "{}" env argument cannot have value <{}>'
+                    raise ValueError(error.format(correct_name, value))
+            break
+        else:
+            value = default
 
-        try:
-            return ast.literal_eval(value)
-        except ValueError:
-            raise cls.IncorrectArgumentValueError(correct_name, value)
+        if value is not DEFAULT_OBJ:
+            return value
 
-    @classmethod
-    def _read_val_from_string(cls, raw_string: str, num: int, file):
-        string = raw_string.lstrip().rstrip()
-        if not string or string.startswith('#'):
-            return ()
-
-        string = string.split('=')
-        if len(string) < 2:
-            raise cls.IncorrectStringError(file, num+1, raw_string)
-
-        if len(string) > 2:
-            string = [string[0], '='.join(string[1:])]
-
-        string = (string[0].rstrip(), string[1].lstrip())
-        return string
+        if len(names) == 1:
+            error = f'"{names[0]}" argument not found'
+        else:
+            error = f'"{names}" arguments not found'
+        raise NameError(error)
 
     def _read_file_into_cache(self, file_path) -> None:
-        with open(file_path) as file:
-            data = file.read()
+        """
+        Reads and parses config file and writes results to own cache.
+        """
 
-        data = filter(
-            None,
-            (self._read_val_from_string(string, i, file_path)
-             for (i, string) in enumerate(data.split('\n')))
-        )
-        data = {key: val for (key, val) in data}
+        def read_val_from_string(raw_string: str) -> FROM_STRING_TYPE:
+            """
+            Parses line by `=` sign and returns variable name with value
+            and success result. If the string begins with `#` it is
+            skipped.
+
+            >>> read_val_from_string('')
+            >>> # ((), True)
+            >>>
+            >>> read_val_from_string('# Comment')
+            >>> # ((), True)
+            >>>
+            >>> read_val_from_string('count = 100')
+            >>> # (('count', '100'), True)
+            >>> read_val_from_string('  string    ="st=ri=ng"   ')
+            >>> # (('string', '"st=ri=ng"'), True)
+            >>>
+            >>> read_val_from_string('line without equal sign')
+            >>> # ((), False)
+            """
+
+            string = raw_string.lstrip().rstrip()
+            if not string or string.startswith('#'):
+                return (), True
+
+            eq_index = string.find('=')
+            if eq_index == -1:
+                return (), False
+
+            name, value = string[:eq_index], string[eq_index + 1:]
+            name, value = name.rstrip(), value.lstrip()
+
+            return (name, value), True
+
+        with open(file_path) as file:
+            text = file.read()
+
+        data = dict()
+        for (i, string) in enumerate(text.split('\n')):
+            result, succ = read_val_from_string(string)
+            if not succ:
+                raise self.IncorrectStringError(file_path, i + 1, string)
+            if result:
+                data[result[0]] = result[1]
+
         self._files_cache[file_path] = data
 
-    def get_arg_from_file(self, name, file_path, default):
+    def get_arg_from_file(self, name: str, file_path, *args, **kwargs):
+        """
+        Reads the file, writes received data to cache and searches the
+        file's cache for desired values.
+        """
+
         if file_path not in self._files_cache:
             self._read_file_into_cache(file_path)
-        return self._get_arg_from_dict(name, self._files_cache[file_path], default)
 
-    def get_arg_from_environ(self, name: STR_OR_ITER, default: any) -> any:
-        return self._get_arg_from_dict(name, os.environ, default)
+        return self.get_arg_from_dict(name, self._files_cache[file_path],
+                                      *args, **kwargs)
 
-    def get_arg_from_envs_file(self, name: STR_OR_ITER, default: any) -> any:
-        return self.get_arg_from_file(name, '.envs', default)
+    @classmethod
+    def get_arg_from_environ(cls, name: STR_OR_ITER, *args, **kwargs) -> any:
+        """
+        A wrapper around `.get_arg_from_dict()` that sets up `os.environ`
+        in the `args_dict: Mapping` variable in advance.
+        """
 
-    def get_arg_from_configs_file(self, name: STR_OR_ITER, default: any) -> any:
-        return self.get_arg_from_file(name, '.configs', default)
+        return cls.get_arg_from_dict(name, os.environ, *args, **kwargs)
+
+    def get_arg_from_envs_file(self, name: STR_OR_ITER, *args, **kwargs) -> any:
+        """
+        A wrapper around `.get_arg_from_file()` that sets up '.envs' file
+        in the `file_path` variable in advance.
+        """
+
+        return self.get_arg_from_file(name, '.envs', *args, **kwargs)
+
+    def get_arg_from_configs_file(self, name: STR_OR_ITER, *args, **kwargs) -> any:
+        """
+        A wrapper around `.get_arg_from_file()` that sets up '.configs'
+        file in the `file_path` variable in advance.
+        """
+
+        return self.get_arg_from_file(name, '.configs', *args, **kwargs)
 
 
 env_parser = EnvParser()
