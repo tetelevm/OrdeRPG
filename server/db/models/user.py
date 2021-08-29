e2b444cac5ead30d0efa7ac23497e4c78305d39f
tetelevm <tetelev99@mail.ru>
2021-08-29 16:07:45 +0300

    Added PersonModel

--- server/db/models/user.py
+++ server/db/models/user.py
@@ -5,10 +5,16 @@ user's business data.
 
 from sqlalchemy import func
 from server.framework.db import (
-    BaseModel, StringField, DateTimeField, RandomStringField, PasswordField,
+    BaseModel,
+    IntegerField,
+    StringField,
+    DateTimeField,
+    RandomStringField,
+    PasswordField,
+    CoefficientField,
 )
 
-__all__ = ['UserModel']
+__all__ = ['UserModel', 'PersonModel']
 
 
 class UserModel(BaseModel):
@@ -16,9 +22,21 @@ class UserModel(BaseModel):
     A user model that only does user authorization, but does it well.
     """
 
-    login = StringField(string_length=50)
+    login = StringField(50)
     pass_hash = PasswordField()
     pepper = RandomStringField(48)
     token = RandomStringField(128)
     created = DateTimeField(default=func.now())
     last_login = DateTimeField()
+
+
+class PersonModel(BaseModel):
+    """A user model containing business logic."""
+
+    type = IntegerField()
+    user = None
+    level = IntegerField()
+    experience = IntegerField()
+    money = IntegerField()
+    rating = IntegerField()
+    kill_ratio = CoefficientField()
