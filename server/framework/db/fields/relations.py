d281d8c3865f0322634ebc7f7ef2027d31a8dca0
tetelevm <tetelev99@mail.ru>
2021-09-19 22:39:37 +0300

    Added kwargs to relations field

--- server/framework/db/fields/relations.py
+++ server/framework/db/fields/relations.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import Tuple
 
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.orm import RelationshipProperty
@@ -60,14 +59,14 @@ class FieldRelationshipClass(ABC):
     @abstractmethod
     def generate_id_column(
             self
-    ) -> Tuple[str, FieldRelationshipClass.column_class]:
+    ) -> tuple[str, FieldRelationshipClass.column_class]:
         pass
 
     @abstractmethod
     def generate_rel_for_c(
             self,
             c_tablename: str,
-    ) -> Tuple[str, FieldRelationshipClass.relation_class]:
+    ) -> tuple[str, FieldRelationshipClass.relation_class]:
         pass
 
     @abstractmethod
@@ -75,7 +74,7 @@ class FieldRelationshipClass(ABC):
             self,
             c_tablename: str,
             c_classname: str,
-    ) -> Tuple[str, FieldRelationshipClass.relation_class]:
+    ) -> tuple[str, FieldRelationshipClass.relation_class]:
         pass
 
     def __str__(self):
@@ -108,12 +107,15 @@ class ForeignKeyColumn(FieldRelationshipColumn):
     def __init__(self, column_type, fk_name, **kwargs):
         self.column_type = column_type
         self.parent_column = ForeignKey(fk_name)
+        kwargs.setdefault('nullable', False)
         super().__init__(**kwargs)
 
 
 class ForeignKeyRelationship(FieldRelationshipRelationship):
-    def __init__(self, clsname: str, backref: str):
-        super().__init__(clsname, back_populates=backref)
+    def __init__(self, clsname: str, as_name: str, **kwargs):
+        kwargs.pop('backref', None)
+        kwargs.pop('back_populates', None)
+        super().__init__(clsname, back_populates=as_name, **kwargs)
 
 
 class ForeignKeyField(FieldRelationshipClass):
@@ -123,51 +125,63 @@ class ForeignKeyField(FieldRelationshipClass):
     def __init__(
             self,
             model,
+            *,
             backref: str = None,
             parent_backref: str = None,
+            column_kwargs: dict = dict(),
+            self_kwargs: dict = dict(),
+            parent_kwargs: dict = dict(),
     ):
         self.model_to = model
         self.parent_name = parent_backref
         self.children_name = backref
+        self.column_kwargs = column_kwargs
+        self.self_kwargs = self_kwargs
+        self.parent_kwargs = parent_kwargs
 
-    def generate_id_column(self) -> Tuple[str, ForeignKeyColumn]:
+    def generate_id_column(self) -> tuple[str, ForeignKeyColumn]:
         parent_tablename = self.model_to.__tablename__
         column_field: FieldDefault = self.model_to.__table__.primary_key.columns[0]
 
         fk_name = f'{parent_tablename}.{column_field.name}'
         fk_field_name = f'{parent_tablename}_{column_field.name}'
-
-        return (
-            fk_field_name,
-            self.column_class(column_field.column_type, fk_name)
+        column = self.column_class(
+            column_field.column_type,
+            fk_name,
+            **self.column_kwargs
         )
+        return fk_field_name, column
 
     def generate_rel_for_c(
             self,
             c_tablename: str,
-    ) -> Tuple[str, ForeignKeyRelationship]:
+    ) -> tuple[str, ForeignKeyRelationship]:
         parent_tablename = self.model_to.__tablename__
 
         parent_name = self.model_to.__name__
-        backref = self.children_name or c_tablename
-
-        return (
-            parent_tablename,
-            self.relation_class(parent_name, backref)
+        self_from_parent_name = self.children_name or c_tablename
+        for_self_rel = self.relation_class(
+            parent_name,
+            self_from_parent_name,
+            **self.self_kwargs
         )
+
+        return parent_tablename, for_self_rel
 
     def generate_rel_for_p(
             self,
             c_tablename: str,
             c_classname: str,
-    ) -> Tuple[str, ForeignKeyRelationship]:
+    ) -> tuple[str, ForeignKeyRelationship]:
         parent_tablename = self.model_to.__tablename__
-        parent_backref = self.parent_name or parent_tablename
+        self_as_name = self.parent_name or parent_tablename
+        for_parent_rel = self.relation_class(
+            c_classname,
+            self_as_name,
+            **self.parent_kwargs
+        )
 
-        return (
-                c_tablename,
-                self.relation_class(c_classname, parent_backref)
-            )
+        return c_tablename, for_parent_rel
 
 
 # ======== ONE TO ONE KEY ========
@@ -177,29 +191,30 @@ class OnoToOneColumn(ForeignKeyColumn):
     pass
 
 
-class OnoToOneRelationship(FieldRelationshipRelationship):
-    def __init__(self, clsname: str, backref: str):
-        super().__init__(clsname, back_populates=backref, uselist=False)
+class OnoToOneRelationship(ForeignKeyRelationship):
+    def __init__(self, clsname: str, as_name: str, **kwargs):
+        kwargs['uselist'] = False
+        super().__init__(clsname, as_name, **kwargs)
 
 
 class OnoToOneField(ForeignKeyField):
     column_class = OnoToOneColumn
     relation_class = OnoToOneRelationship
 
-    def generate_id_column(self) -> Tuple[str, OnoToOneColumn]:
+    def generate_id_column(self) -> tuple[str, OnoToOneColumn]:
         return super().generate_id_column()
 
     def generate_rel_for_c(
             self,
             c_tablename: str,
-    ) -> Tuple[str, OnoToOneRelationship]:
+    ) -> tuple[str, OnoToOneRelationship]:
         return super().generate_rel_for_c(c_tablename)
 
     def generate_rel_for_p(
             self,
             c_tablename: str,
             c_classname: str,
-    ) -> Tuple[str, OnoToOneRelationship]:
+    ) -> tuple[str, OnoToOneRelationship]:
         return super().generate_rel_for_p(c_tablename, c_classname)
 
 
