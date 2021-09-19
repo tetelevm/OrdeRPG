b8dae4c72e0dbefae72f5b2bca9e2f12ac7cb843
tetelevm <tetelev99@mail.ru>
2021-09-19 22:43:55 +0300

    Updated user model settings

--- server/db/models/user.py
+++ server/db/models/user.py
@@ -25,23 +25,23 @@ class UserModel(BaseModel):
     A user model that only does user authorization, but does it well.
     """
 
-    login = StringField(50)
+    login = StringField(50, nullable=False)
     pass_hash = PasswordField()
     pepper = RandomStringField(48)
     token = RandomStringField(128)
     created = DateTimeField(default=func.now())
     last_login = DateTimeField()
-    is_deleted = BooleanField()
+    is_deleted = BooleanField(default=False, nullable=False)
 
 
 class PersonModel(BaseModel):
     """A user model containing business logic."""
 
-    type = IntegerField()
+    type = IntegerField(nullable=False)
     user = OnoToOneField(UserModel)
-    level = IntegerField()
-    experience = IntegerField()
-    money = IntegerField()
-    rating = IntegerField()
-    kill_ratio = CoefficientField()
-    fights_count = PositiveIntegerField()
+    level = IntegerField(default=1, nullable=False)
+    experience = IntegerField(default=0, nullable=False)
+    money = IntegerField(default=0, nullable=False)
+    rating = IntegerField(default=0, nullable=False)
+    kill_ratio = CoefficientField(default=0., nullable=False)
+    fights_count = PositiveIntegerField(default=0, nullable=False)
