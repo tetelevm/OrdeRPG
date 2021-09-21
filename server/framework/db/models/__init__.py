c90d3359706f07f347ac313a5441265a8c22f833
tetelevm <tetelev99@mail.ru>
2021-09-21 23:39:35 +0300

    (framework) Small fixes for imports

--- server/framework/db/models/__init__.py
+++ server/framework/db/models/__init__.py
@@ -1,9 +1,11 @@
 from .default import __all__ as __default__all__
 from . base import _all_ as _base_all_
+from . utils import __all__ as __utils_all__
 
 from .default import *
 from .base import *
+from .utils import *
 
 
-_all_ = _base_all_
+_all_ = _base_all_ + __utils_all__
 __all__ = _all_ + __default__all__
