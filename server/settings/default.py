f396c5ed4c869e9e38518a00855d85b08fe308d0
tetelevm <tetelev99@mail.ru>
2021-09-21 09:54:42 +0300

    (project) Added hash_salt to project settings

--- server/settings/default.py
+++ server/settings/default.py
@@ -3,6 +3,7 @@ Settings that should always be set, regardless of the others.
 """
 
 from pathlib import Path
+from server.lib.classes import env_parser as envs
 from server.framework.settings import settings as s
 
 __all__ = []
@@ -11,3 +12,5 @@ __all__ = []
 # Directory with `main.py`
 s.home_path = Path(__file__).parent.parent.parent
 s.server_path = (Path() / 'server').resolve()
+
+s.hash_salt = envs.get_arg_from_envs_file('hash_salt')
