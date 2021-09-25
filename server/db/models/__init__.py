4b52cefd21da6a6b5f1a24d174f8fec25eacd930
tetelevm <tetelev99@mail.ru>
2021-09-25 22:47:33 +0300

    (project) Fix imports

--- server/db/models/__init__.py
+++ server/db/models/__init__.py
@@ -2,13 +2,20 @@
 Models to be stored in the database.
 """
 
+from .fight import __all__ as __fight_all__
 from .item import __all__ as __item_all__
 from .location import __all__ as __location_all__
 from .user import __all__ as __user_all__
 
+from .fight import *
 from .item import *
 from .location import *
 from .user import *
 
 
-__all__ = __user_all__ + __item_all__ + __location_all__
+__all__ = (
+    __user_all__
+    + __fight_all__
+    +__item_all__
+    + __location_all__
+)
