787c16600e029daf345ce3be8d01a532d4d3a14c
tetelevm <tetelev99@mail.ru>
2022-02-07 20:11:16 +0300

    Removed the ability to import from the `db` submodule

--- framework/db/__init__.py
+++ framework/db/__init__.py
@@ -2,14 +2,6 @@
 The main addition to SQLAlchemy that is used in the project.
 """
 
-from .models import __all_for_module__ as __models_all__
-from .fields import __all_for_module__ as __fields_all__
-from .peel import __all_for_module__ as __peel_all__
 
-from .models import *
-from .fields import *
-from .peel import *
-
-
-__all_for_module__ = __peel_all__ + __fields_all__ + __models_all__
+__all_for_module__ = []
 __all__ = __all_for_module__
